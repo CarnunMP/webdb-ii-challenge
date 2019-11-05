401a7fb35660bfb72175e3b720792a46d49c5ed7
CarnunMP <carnun@hotmail.co.uk>
2019-11-05 15:47:17 +0000

    knex and sqlite3 installed; console.log of process.end.NODE_ENV added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const carsRouter = require('./cars/carsRouter');
 
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
+
 const server = express();
 
 server.use(express.json());
